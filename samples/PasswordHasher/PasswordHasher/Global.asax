30dfe0c0f222b35893a7aae40047741659947b30
Brock Allen <brockallen@gmail.com>
2014-01-23 18:04:06 -0500

    initial sample

---
+++ samples/PasswordHasher/PasswordHasher/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PasswordHasher.MvcApplication" Language="C#" %>
---
+++ samples/PasswordHasher/PasswordHasher/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PasswordHasher.Startup))]
+namespace PasswordHasher
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
