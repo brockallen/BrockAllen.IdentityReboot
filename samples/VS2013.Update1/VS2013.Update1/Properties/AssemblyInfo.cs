bb7adf17a7a7da0e81d655185187183221b4c11c
Brock Allen <brockallen@gmail.com>
2014-02-07 17:36:12 -0500

    fix namespace

--- samples/VS2013.Update1/VS2013.Update1/Properties/AssemblyInfo.cs
+++ samples/VS2013.Update1/VS2013.Update1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VS2013._1")]
+[assembly: AssemblyTitle("VS2013.Update1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VS2013._1")]
+[assembly: AssemblyProduct("VS2013.Update1")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b7f06c6e-78ca-4237-9f88-6aa6cbcaba70")]
+[assembly: Guid("4a139be4-67a5-4fca-83db-e5260b2114da")]
 
 // Version information for an assembly consists of the following four values:
 //
