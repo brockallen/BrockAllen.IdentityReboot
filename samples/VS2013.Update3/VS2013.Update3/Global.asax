907cb586d13296617409db6d106911f2d3df6fa4
Brock Allen <brockallen@gmail.com>
2014-09-26 18:47:57 -0400

    add sample MVC project with necessary changes for AspId v2.1

---
+++ samples/VS2013.Update3/VS2013.Update3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2013.Update3.MvcApplication" Language="C#" %>
---
+++ samples/VS2013.Update3/VS2013.Update3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2013.Update3.Startup))]
+namespace VS2013.Update3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
